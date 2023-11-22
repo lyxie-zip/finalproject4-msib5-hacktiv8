2181aaa028c08388fc9f9cd8f7ea24f33a4f1a64
Valdy Ramadhan <74580612+lyxie-zip@users.noreply.github.com>
2023-11-23 03:01:22 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/elysiamori/finalproject4-hacktiv8-msib5/controllers"
+	"github.com/elysiamori/finalproject4-hacktiv8-msib5/database"
+	"github.com/elysiamori/finalproject4-hacktiv8-msib5/repositories"
+	"github.com/elysiamori/finalproject4-hacktiv8-msib5/routers"
+)
+
+func main() {
+	db, err := database.Config()
+	if err != nil {
+		panic(err)
+	}
+
+	// user
+	userRepository := repositories.NewUserRepository(db)
+	userController := controllers.UserController{UserRepo: *userRepository}
+
+	// category
+	categoryRepository := repositories.NewCategoryRepository(db)
+	categoryController := controllers.CategoryController{CategoryRepo: *categoryRepository}
+
+	// product
+	productRepository := repositories.NewProductRepository(db)
+	productController := controllers.ProductController{ProductRepo: *productRepository}
+
+	// transaction
+	transactionRepository := repositories.NewTransactionRepository(db)
+	transactionController := controllers.TransactionController{TransactionRepo: *transactionRepository}
+
+	r := routers.StartApp(&userController, &categoryController, &productController, &transactionController)
+
+	r.Run(":3000")
+}
